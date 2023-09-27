b62553498f000a8e49d571ea8a3d9c5cb193cb91
TSezari Kvelashvili <cez3xxx@gmail.com>
2023-09-27 19:28:16 +0400

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="container">
+                Home
+            </div>
+        </div>
+    )
+}
+
+export default Home
